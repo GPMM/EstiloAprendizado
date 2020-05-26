947130b7fc15763c4951e63d971192ec03f80e5e
YasminCardoso <yasmincardoso@users.noreply.github.com>
2020-05-26 17:44:01 -0300

    Gráfico Com Resultado
    Colocar gráfico de radar na tela do resultado.

--- PerfilDeAprendizado/app/src/main/java/com/ppa/perfildeaprendizado/ResultadoActivity.java
+++ PerfilDeAprendizado/app/src/main/java/com/ppa/perfildeaprendizado/ResultadoActivity.java
@@ -8,10 +8,14 @@ import android.text.Layout;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.anychart.anychart.AnyChart;
+import com.anychart.anychart.AnyChartView;
+import com.anychart.anychart.*;
 import com.ppa.perfildeaprendizado.data.model.Aluno;
 import com.ppa.perfildeaprendizado.task.InserirAlunoTask;
 import com.ppa.perfildeaprendizado.ui.login.LoginActivity;
 
+import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
 
 public class ResultadoActivity extends AppCompatActivity {
@@ -20,6 +24,7 @@ public class ResultadoActivity extends AppCompatActivity {
     private ProgressBar reflexivoBar;
     private ProgressBar teoricoBar;
     private ProgressBar pragmaticoBar;
+    private AnyChartView radarChart;
 
     private Aluno aluno = new Aluno();
 
@@ -33,6 +38,7 @@ public class ResultadoActivity extends AppCompatActivity {
         reflexivoBar = findViewById(R.id.reflexivoBar);
         teoricoBar = findViewById(R.id.teoricoBar);
         pragmaticoBar = findViewById(R.id.pragmaticoBar);
+        radarChart = findViewById(R.id.radarChart);
 
         aluno = (Aluno) getIntent().getSerializableExtra(Aluno.class.getSimpleName());
 
@@ -63,6 +69,28 @@ public class ResultadoActivity extends AppCompatActivity {
         reflexivoBar.setProgress(aluno.getPerfilReflexivo());
         teoricoBar.setProgress(aluno.getPerfilTeorico());
         pragmaticoBar.setProgress(aluno.getPerfilPragmatico());
+        ChartsRadar radar = AnyChart.radar();
+        radar.setTitle(aluno.getNome());
+        radar.getYScale().setMinimum(0d);
+        radar.getYScale().setMinimumGap(0d);
+        radar.getXAxis().getLabels().setPadding(5d,5d,5d,5d);
+    }
+
+    private ArrayList getData(){
+        ArrayList<DataEntry> entries = new ArrayList<>();
+        DataEntry ativo = new DataEntry();
+        DataEntry reflexivo = new DataEntry();
+        DataEntry teorico = new DataEntry();
+        DataEntry pragmatico = new DataEntry();
+        ativo.setValue(getString(R.string.ativo), aluno.getPerfilAtivo());
+        reflexivo.setValue(getString(R.string.reflexivo), aluno.getPerfilReflexivo());
+        teorico.setValue(getString(R.string.teorico), aluno.getPerfilTeorico());
+        pragmatico.setValue(getString(R.string.pragmático), aluno.getPerfilPragmatico());
+        entries.add(ativo);
+        entries.add(reflexivo);
+        entries.add(teorico);
+        entries.add(pragmatico);
+        return entries;
     }
 
     public void mostrarResultadosQuestionario(){
