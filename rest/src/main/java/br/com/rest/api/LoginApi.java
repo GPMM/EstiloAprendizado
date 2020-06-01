e7878273a452b5e224e492c5a2dfb745a74a000a
Fernando Godoy <nanu.godoy@hotmail.com>
2020-06-01 18:42:17 -0300

    limpeza de imports

--- rest/src/main/java/br/com/rest/api/LoginApi.java
+++ rest/src/main/java/br/com/rest/api/LoginApi.java
@@ -1,21 +1,12 @@
 package br.com.rest.api;
 
-import java.util.List;
-
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import br.com.rest.model.dao.ProfessorDAO;
-import br.com.rest.model.dao.QuestionarioDAO;
-import br.com.rest.model.dao.RELQuestionarioTurmaDAO;
-import br.com.rest.model.dao.TurmaDAO;
 import br.com.rest.model.dto.LoginDTO;
 import br.com.rest.model.entity.AlunoEntity;
-import br.com.rest.model.entity.ProfessorEntity;
-import br.com.rest.model.entity.QuestionarioEntity;
-import br.com.rest.model.entity.TurmaEntity;
 import br.com.rest.services.AlunoServices;
 
 @Path("/login")
