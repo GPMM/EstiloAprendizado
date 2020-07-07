1744c0eedb48b1829c2b68279be080aceacc0b9b
Fernando Godoy <nanu.godoy@hotmail.com>
2020-07-07 18:17:47 -0300

    adição de endpoint de buscar questionários por aluno, baseado em seus grupos (turma, futuramente nível de ensino)

--- rest/src/main/java/br/com/rest/api/QuestionarioApi.java
+++ rest/src/main/java/br/com/rest/api/QuestionarioApi.java
@@ -8,26 +8,17 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
-import com.sun.istack.NotNull;
-
-import br.com.rest.model.dto.EstiloAlunoDTO;
-import br.com.rest.services.EstiloAlunoServices;
+import br.com.rest.model.entity.QuestionarioEntity;
+import br.com.rest.services.QuestionarioServices;
 
 @Path("/questionario")
 public class QuestionarioApi {
-/*
+
 	@GET
-	@Path("dados-aluno")
+	@Path("grupos")
 	@Produces(MediaType.APPLICATION_JSON)
-	@SuppressWarnings("unchecked")
-	public EstiloAlunoDTO consultarDadosAlunoQuestionario(@NotNull @QueryParam(value = "matricula") String matricula, @NotNull @QueryParam(value = "idQuest") Long idQuestionario) {
-		EstiloAlunoDTO retorno = null;
-		List<EstiloAlunoDTO> consultar = (List<EstiloAlunoDTO>) EstiloAlunoServices.consultar(idQuestionario, matricula, null, null, null, null);
-		List<EstiloAlunoDTO> listaConsulta = consultar;
-		if(listaConsulta != null && listaConsulta.size() == 1)
-			retorno = listaConsulta.get(0);
-		
-		return retorno;
-	}*/
+	public List<QuestionarioEntity> consultarQuestionariosPorGruposAluno(@QueryParam(value = "matricula") String matricula) {
+		return QuestionarioServices.buscarQuestionariosPorGruposAluno(matricula);
+	}
 
 }
--- rest/src/main/java/br/com/rest/services/QuestionarioServices.java
+++ rest/src/main/java/br/com/rest/services/QuestionarioServices.java
@@ -1,16 +1,25 @@
 package br.com.rest.services;
 
-import javax.persistence.NoResultException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
+import javax.persistence.NoResultException;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+
+import br.com.rest.model.dao.AlunoDAO;
+import br.com.rest.model.dao.GrupoAlunoDAO;
 import br.com.rest.model.dao.PersistenceManager;
 import br.com.rest.model.dao.QuestionarioDAO;
-import br.com.rest.model.dto.EstiloAlunoDTO;
-import br.com.rest.model.entity.EstiloAlunoEntity;
+import br.com.rest.model.entity.AlunoEntity;
 import br.com.rest.model.entity.QuestionarioEntity;
 
 public class QuestionarioServices {
 	
 	private static QuestionarioDAO questionarioDao = QuestionarioDAO.getInstance();
+	private static GrupoAlunoDAO grupoAlunoDao = GrupoAlunoDAO.getInstance();
+	private static AlunoDAO alunoDao = AlunoDAO.getInstance();
 	
 	
 	public static Boolean incluirQuestionario(QuestionarioEntity questionario) {
@@ -36,5 +45,30 @@ public class QuestionarioServices {
 				return false;
 			}
 		}
+	}
+	
+	public static List<QuestionarioEntity> buscarQuestionariosPorGruposAluno(String matricula){
+		List<QuestionarioEntity> questionariosBanco = new ArrayList<QuestionarioEntity>();
+		AlunoEntity aluno = null;
+		try {
+			aluno = alunoDao.buscarByMatricula(matricula);
+		} catch(NoResultException e) {
+			throw new WebApplicationException(
+				      Response.status(Response.Status.NO_CONTENT)
+				        .entity("Aluno não existe no banco")
+				        .build()
+				    );
+		}
+		
+		try {
+			questionariosBanco = grupoAlunoDao.findQuestionariosByGruposAluno(aluno);
+		} catch(NoResultException e) {
+			throw new WebApplicationException(
+				      Response.status(Response.Status.NO_CONTENT)
+				        .entity("Aluno não está em nenhum grupo de alunos com questionários disponíveis")
+				        .build()
+				    );
+		}
+		return questionariosBanco;
 	}
 }
